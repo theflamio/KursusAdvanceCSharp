3783d8639e782ba0d055cff176bb98c8ceac8ab6
admin <admin@TA-822-01.kursdom.net>
2019-06-24 09:44:22 +0200

    Add project files.

---
+++ KursusAdvanceCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KursusAdvanceCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
