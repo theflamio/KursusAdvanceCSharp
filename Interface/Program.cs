bba4a607399da7bfa7ec0ad79a39bf8dfc1c1a08
theflamio <flamekfor@gmail.com>
2019-06-25 09:29:54 +0200

    Day 2 - interfaces

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -6,7 +6,44 @@ namespace InterfaceTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* opgave 110-1 eget interface */
+            IDbFunktioner[] ar = new IDbFunktioner[4];
+            ar[0] = new Hund();
+            ar[1] = new Ubåd();
+            ar[2] = new Ubåd();
+            ar[3] = new Ubåd();
+
+            foreach (var item in ar)
+            {
+                item.Gem();
+            }
         }
+    }
+    class Hund : IDbFunktioner
+    {
+
+        public string Navn { get; set; }
+
+        public void Gem()
+        {
+            Console.WriteLine("Gemmer Hund");
+        }
+    }
+
+    class Ubåd : IDbFunktioner
+    {
+
+        public int Nummer { get; set; }
+        public double Tubine { get; set; }
+
+        public void Gem()
+        {
+            Console.WriteLine("Gemmer Ubåd");
+        }
+    }
+
+    interface IDbFunktioner
+    {
+        void Gem();
     }
 }
---
+++ Opgaver_interface_110_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Opgaver_interface_110_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund[] hunde = new Hund[4];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
+            hunde[1] = new Hund() { Alder = 5, Navn = "Lady" };
+            hunde[2] = new Hund() { Alder = 7, Navn = "Andy" };
+            hunde[3] = new Hund() { Alder = 3, Navn = "Rolf" };
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Navn);
+            }
+        }
+
+        class Hund : IComparable<Hund>
+        {
+            public string Navn { get; set; }
+            public int Alder { get; set; }
+
+            public int CompareTo(Hund other)
+            {
+                // Samligne referancen af navnet her med en anden
+                return this.Navn.CompareTo(other.Navn);
+            }
+        }
+    }
+}
