af34614f5baaa85669398f600bf409fb4dfcd93e
theflamio <flamekfor@gmail.com>
2019-06-27 15:23:07 +0200

    Day 4 slut

---
+++ Opgaver_Async/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Opgaver_Async
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
+            s.Start();
+            Console.WriteLine("Start");
+
+            Task t1 = Task.Run(() =>
+            {
+                Console.WriteLine("Sleep");
+                Thread.Sleep(500);
+            });
+
+            Task t2 = Task.Run(() =>
+            {
+                Console.WriteLine("Sleep");
+                Thread.Sleep(500);
+            });
+
+            Task t3 = Task.Run(() =>
+            {
+                Console.WriteLine("Sleep");
+                Thread.Sleep(500);
+            });
+
+            await Task.WhenAll(t1, t2, t3);
+            Console.WriteLine("Slut");
+            s.Stop();
+            Console.WriteLine($"Tid: {s.ElapsedMilliseconds}");
+        }
+    }
+}
